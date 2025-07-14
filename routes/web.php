d9893a5720d42bac4bc339993653a959559fdf91
purinahdatul2004 <purinahdatul133@gmail.com>
2025-07-14 21:04:36 +0700

    Resolve merge conflict

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
+
 Route::get('/contact', function () {
     return view('contact');
-<<<<<<< HEAD
-})->name('contact');
-
-=======
 });
->>>>>>> 3b6255a30fcfc401b7ba6a7a21dead40fdab71f0
 Route::get('/services', [\App\Http\Controllers\ServiceController::class, 'index']);
