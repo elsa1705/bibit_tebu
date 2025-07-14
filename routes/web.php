6319ef9423005fb58b95c9591c7ce4fec6134b18
Della Marsela <dellamarsela6@gmail.com>
2025-07-14 21:03:28 +0700

    uxbau

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
 Route::get('/contact', function () {
     return view('contact');
 });
